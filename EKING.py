3e90334201546685b0e0d8fed46ec4b185f1e74b
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-17 18:33:14 +0430

    Create EKING.py

---
+++ EKING.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import time
+os.system('clear')
+print('\033[1;32mEKING COMMAND IN UPDATE WAIT FOR UPDATE')
+time.sleep(1)
+print('\033[1;32mTHANKS FOR USE EKING')
+time.sleep(1)
+exit()
