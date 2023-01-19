5c7bd9508a91d7954b784f334258b4bd1e418e80
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-19 16:08:37 +0430

    Delete EKING.py

--- EKING.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import time
-os.system('clear')
-print('\033[1;32mEKING COMMAND IN UPDATE WAIT FOR UPDATE')
-time.sleep(1)
-print('\033[1;32mTHANKS FOR USE EKING')
-time.sleep(1)
-exit()
