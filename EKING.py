c516e272eaf47f3c137c711ac0ed1605b0b098be
MR-ELYAS <120295050+MR-ELYAS@users.noreply.github.com>
2023-01-26 23:57:28 +0430

    Update EKING.py

--- EKING.py
+++ EKING.py
@@ -1,1 +1,8 @@
-import XD
+import os,sys,time
+
+def menu():
+os.system("clear")
+print('\033[1;32mEKING TOOL ON UPDATE');time.sleep(2)
+print('\033[1;32mWAIT FOR UPDATE ');time.sleep(2)
+input("\n PRESS ENTER TO GO BACK")
+menu()
